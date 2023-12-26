6160b57e4f4dc15a8d4cf7df93f4caff3ead57f0
gciurpita <greg.ciurpita@gmail.com>
2023-12-26 12:00:10 -0500

    display images on matching green background

---
+++ CtcPanel.java
@@ -0,0 +1,299 @@
+// CTC Panel
+
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+import java.util.*;
+
+import java.awt.image.BufferedImage;
+import java.awt.event.*;
+import java.awt.geom.AffineTransform;
+import javax.imageio.ImageIO;
+
+
+@SuppressWarnings ("serial")
+
+// -----------------------------------------------------------------------------
+// bitmap description - id, label, ...
+
+class Bmp {
+    Image   img;
+    public Bmp (String filename) {
+    }
+}
+
+// -----------------------------------------------------------------------------
+// display railroad schematic and USS switch & signal levers
+
+public class CtcPanel extends JPanel
+        implements MouseListener, KeyListener
+{
+    JFrame frame = new JFrame ();
+
+    private static final String Title           = "Pacific Southern Railway";
+
+    public static final int     CANVAS_WIDTH    = 800;
+    public static final int     CANVAS_HEIGHT   = 400;
+
+    private int                 dbg             = 0;
+
+    private Bmp                 bg;
+    private int                 bgSize          = 0;
+
+    private static final int    MAX_BMP         = 200;
+    private static final int    MAX_ELEM        = 5000;
+    private static final int    MAX_RECT        = 50;
+    private static final int    MAX_TEXT        = 1000;
+
+    private String              cfgFile;
+    private Bmp                 bmp []          = new Bmp  [MAX_BMP];
+    private int                 bmpSize         = 0;
+
+    // ------------------------------------------------------------------------
+    // constructor loads the configuration files and
+    //   determines the initial display geometry
+
+    public CtcPanel (String configuration)
+            throws FileNotFoundException, IOException, IllegalArgumentException
+    {
+        byte[]  buf     = new byte [10];
+
+        addKeyListener   (this);
+        addMouseListener (this);
+
+
+        cfgFile  = configuration;
+        loadCfg (configuration);
+
+        this.setPreferredSize (new Dimension (CANVAS_WIDTH, CANVAS_HEIGHT));
+
+        frame.setContentPane (this);
+        frame.pack ();
+        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        frame.setVisible (true);
+
+        // position app near top center of screen
+        Rectangle r = frame.getBounds();        // window size
+        frame.setBounds (900, 0, r.width, r.height);
+    }
+
+    // --------------------------------
+    public void keyPressed (KeyEvent e)
+    {
+        int     code    = e.getKeyCode();
+        switch (code)  {
+            case KeyEvent.VK_LEFT:
+                break;
+
+            case KeyEvent.VK_RIGHT:
+                break;
+
+            default:
+                break;      // ignore char keypresses belwo
+        }
+    }
+
+    // --------------------------------
+    public void keyReleased (KeyEvent e)
+    {
+        // ignore
+    }
+
+    // --------------------------------
+    public void keyTyped (KeyEvent e)
+    {
+        byte[]  buf     = new byte [10];
+
+        char    c = e.getKeyChar();
+        switch (c)  {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+                break;
+
+            case 'r':
+                repaint();
+                break;
+
+            case '?':
+                System.out.format ("key commands:\n");
+                System.out.format ("    [0-9] show tower\n");
+                System.out.format ("    r - repaint\n");
+                System.out.format ("   -> - + tower (right arrow)\n");
+                System.out.format ("   <- - - tower (left arrow)\n");
+                System.out.format ("    ? - this help display\n");
+                break;
+
+            default:
+                System.out.format ("keyTyped: %c unexpected\n", c);
+                break;
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    // open and loads a bitmap file
+
+    private void loadImage (String imgFileName, Bmp bmp)
+        throws IllegalArgumentException
+    {
+        System.out.format ("      loadImage: %s\n", imgFileName);
+
+        URL url = getClass ().getClassLoader ().getResource (imgFileName);
+
+        if  (url == null) {
+            throw new IllegalArgumentException ("cannot find " + imgFileName);
+        } else {
+            try {
+                bmp.img  = ImageIO.read (url);
+            } catch  (IOException ex) {
+                ex.printStackTrace ();
+            }
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    // reads a configuration file, processing different record types
+    //   recursively read additonal files, create new Bmp and Elem data
+
+    private void loadCfg (String configuration)
+            throws FileNotFoundException, IOException
+    {
+        System.out.format (" loadCfg: %s\n", configuration);
+
+        BufferedReader br = new BufferedReader(new FileReader(configuration + ".cfg"));
+        String         line;
+
+        while ((line = br.readLine()) != null)  {
+            String[]    fields = line.split("  *");
+            System.out.format ("   loadCfg: %s - %s\n", line, fields [0]);
+
+            // -----------------------------------------------------------------
+            // load icon
+
+            if (fields[0].equals("icon") || fields[0].equals("bg"))  {
+                if (fields.length < 4)  {
+                    throw new IllegalArgumentException (
+                        "Error - loadCfg: icon type id  <filename>\n");
+                }
+
+                bmp[bmpSize] = new Bmp (fields[3]);
+                loadImage (fields[3], bmp[bmpSize++]);
+            }
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    // main processes command line arguments settign the debug level and
+
+    public static void main (String[] args)
+            throws FileNotFoundException, IOException, IllegalArgumentException
+    {
+        for (int i = 0; i < args.length; i++)
+            System.out.format ("main: %d %s\n", i, args[i]);
+
+        CtcPanel disp = new CtcPanel (args[0]);
+     // CtcPanel disp = new CtcPanel ("Resources/ctcTiles");
+        System.out.format ("  main: done\n");
+    }
+
+    // ------------------------------------------------------------------------
+    // process mouse press, search for element closest to mouse location
+
+    public void mousePressed (MouseEvent ev)
+    {
+        int  p       = ev.getX();
+        int  q       = ev.getY();
+
+
+        requestFocusInWindow ();
+    }
+
+    // ------------------------------------------------------------------------
+    // ignore these mouse events
+    public void mouseClicked  (MouseEvent e) { }
+    public void mouseEntered  (MouseEvent e) { }
+    public void mouseExited   (MouseEvent e) { }
+    public void mouseReleased (MouseEvent e) { }
+
+    // ------------------------------------------------------------------------
+    // draws bmps
+
+    private void paintBmps (Graphics2D g2d, AffineTransform transform)
+    {
+        int     b;
+        int     x0  = 0;
+        int     y0  = 0;
+        int     x;
+        int     y;
+
+        System.out.format ("paintBmps:\n");
+
+        g2d.setColor (Color.white);
+        g2d.drawLine (0, 100, CANVAS_WIDTH, 100);
+
+        for (b = 0; b < 5; b++)  {
+            System.out.format ("  paintBmps: %d  wid %d\n",
+                    b, bmp [b].img.getWidth(null));
+
+            x  = b * 100;
+            y  = 100;
+
+            transform.translate (x - x0, y - y0);
+
+            x0 = x;
+            y0 = y;
+
+            g2d.drawImage (bmp [b].img, transform, this);
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    // draw grid
+
+    private void paintGrid (Graphics2D g2d, AffineTransform transform)
+    {
+        final int Col = 160;
+        final int Row =  55;
+
+        System.out.format ("paintGrid: wid %d, ht %d\n",
+            bmp [2].img.getWidth (null), bmp [2].img.getHeight (null) );
+
+        for (int row = 1; row <= Row; row++)  {
+        }
+
+        for (int col = 1; col <= Col; col++)  {
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    // redraw the screen -- recalculate the scale factors, redraw the
+    //   background bitmaps and call paintElems() to redraw features
+
+    @Override
+    public void paintComponent (Graphics g)
+    {
+        Graphics2D  g2d = (Graphics2D) g;
+        Rectangle   r   = frame.getBounds();
+
+        // set background to black
+     // g2d.setColor (Color.black);
+     // g2d.setColor (new Color.parseColor("#316b35"));
+        g2d.setColor (new Color(49, 107, 53));
+        g2d.fillRect (0, 0, r.width, r.height);
+
+        // -----------------------------------------------
+        AffineTransform transform = new AffineTransform ();
+
+        int         x0  = 0;
+        int         y0  = 0;
+        int         x;
+        int         y;
+
+        paintBmps     (g2d, transform);
+        paintGrid     (g2d, transform);
+    }
+}
---
+++ Makefile
@@ -0,0 +1,52 @@
+
+# LINT_FLAG  = -Xlint:deprecation
+
+    Classes = $(patsubst %.java, %.class, $(Srcs))
+    Srcs    = $(shell echo *java)
+    Targ    = CtcPanel
+
+    Jar     = $(Targ).jar
+    Opt     = -p0
+    Opt     = -p3 -fc0.cmd
+
+    Sopt    = cyc1:50,psi1,TE,mph,lbs1:10
+    Topt    = Tsec=1
+
+# --------------------------------------------------------------------
+%.class : %.java
+		javac $(LINT_FLAG) $<
+
+% : %.class
+		java $@ $(Args)
+
+# --------------------------------------------------------------------
+run : $(Classes)
+		java $(Targ)  Resources/ctcTiles | tee $(Targ).out
+
+all : $(Classes)
+
+cmd : $(Classes)
+		java $(Targ) $(Opt) | tee $(Targ).out
+
+jar : all
+		jar cmvf          MANIFEST.MF $(Targ).jar *.class
+#		jar cmvf META-INF/MANIFEST.MF $(Targ).jar *.class
+
+runJar : jar
+		java -jar $(Targ).jar
+
+view : $(Jar)
+		appletviewer driver.html
+
+xgr :
+		plot.k $(Topt) S=$(Sopt) $(Targ).out | tee $(Targ).xgr
+
+xgr2 :
+		plot.k  S=TE,mph,lbs1:10 $(Targ).out | tee $(Targ).xgr
+
+# --------------------------------------------------------------------
+neat :
+		rm -f *~ *.class
+
+clean : neat
+		rm -f *.jar
