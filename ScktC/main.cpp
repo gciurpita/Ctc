3ae7f50d21430d4bd9d112dcaf3b5acb82183b57
gciurpita <greg.ciurpita@gmail.com>
2023-12-29 10:19:33 -0500

    add pktDecode and dump in main.cpp

--- ScktC/main.cpp
+++ ScktC/main.cpp
@@ -1,6 +1,7 @@
 // 
 
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <getopt.h>
 #include <unistd.h>
@@ -13,6 +14,85 @@ char        *progname;
 unsigned int debug = 0;
 unsigned int flag  = 0;
 
+int col;
+int pos;
+
+// --------------------------------------------------------------------
+enum {
+    PktString = 2,
+    PktStart,
+    PktLvrTo,
+    PktLvrSig,
+    PktHelp,
+};
+
+const char *pktTypeStr [] = {
+    "PktId0",
+    "PktId1",
+    "PktString",
+    "PktStart",
+    "PktLvrTo",
+    "PktLvrSig",
+    "PktHelp",
+};
+
+const char *lvrStr [] = {
+    "Left",
+    "Right",
+    "Center",
+};
+
+// ----------------------------
+void
+pktDecode (
+    uint8_t *buf,
+    int      nByte )
+{
+    uint8_t  type = buf [0];
+    uint8_t  len  = buf [1];
+
+    switch (type) {
+    case PktLvrSig:
+    case PktLvrTo:
+        if (len < 4)
+            printf (" %s: incomplete %s pkt - %d bytes\n",
+                                        pktTypeStr [type], len);
+        else  {
+            col = buf [2] + 1;
+            pos = buf [3];
+            printf (" %s: %s col %d, pos %s\n", __func__,
+                           pktTypeStr [type], col, lvrStr [pos]);
+        }
+        break;
+
+    case PktString:
+        printf (" %s: sring - %s\n", __func__, & buf [2]);
+        break;
+
+    case PktStart:
+        printf (" %s: start\n", __func__);
+        break;
+
+    default:
+        printf (" %s: invalid pkt - %d\n", __func__, type);
+        break;
+
+    }
+};
+
+// --------------------------------------------------------------------
+void
+pktDump (
+    uint8_t *buf,
+    int      nByte )
+{
+    printf ("%s:", __func__);
+    printf (" %d %9s", buf [0], pktTypeStr [buf [0]]);
+    printf (", len %d", buf [1]);
+    for (int n = 2; n < nByte; n++)
+        printf (" %02x", buf [n]);
+    printf ("\n");
+}
 
 // --------------------------------------------------------------------
 void
@@ -20,12 +100,19 @@ shell (void)
 {
     sckt.scktOpen (PORT, 1);
 
-    unsigned char buf [BUFSIZ];
-    int           nByte;
+    uint8_t  buf [BUFSIZ];
+    int      nByte;
 
     while (true)  {
         nByte = sckt.receive (buf, BUFSIZ, 0);
-        printf ("%s: nByte %d\n", __func__, nByte);
+        if (nByte)
+#if 0
+            printf ("%s: nByte %d\n", __func__, nByte);
+#elif 0
+            pktDump (buf, nByte);
+#else
+            pktDecode (buf, nByte);
+#endif
         sleep (1);
     }
 }
