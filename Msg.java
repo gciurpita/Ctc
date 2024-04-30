2b87ea356d47f471ced78088d398bb81913cec6a
gciurpita <greg.ciurpita@gmail.com>
2024-04-30 17:57:42 -0400

    deleted older Ctc code

--- Msg.java
+++
@@ -1,8 +0,0 @@
-
-// ---------------------------------------------------------
-public class Msg   {
-    char    type;   // 'T' - turnout, 'S' - signal, '_' - unused
-    int     id;
-    char    state;  // T - 'N'/'R', S - 'S'/'C'
-}
-
